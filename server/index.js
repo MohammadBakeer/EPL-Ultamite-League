5b2042dcbfc971fb2194d8fdd43630e7e42ca863
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-08-15 20:59:39 -0500

    port

--- server/index.js
+++ server/index.js
@@ -95,5 +95,5 @@ app.get('/verify-email', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
