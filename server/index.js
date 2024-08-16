ac501aa2b742f07de00a137c89c5696d26c1842d
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-08-16 18:19:27 -0500

    trying public dir in index.js

--- server/index.js
+++ server/index.js
@@ -40,6 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions)); // Apply CORS middleware with options
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 // Token update route
