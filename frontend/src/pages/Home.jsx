a3bef23ec08680bcffa23b13b497cdc6324c95be
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-08-16 21:04:19 -0500

    test without large card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { decodeJWT } from "../jwtUtils.js";
 import Field from "../components/field/Field.jsx"; // Update the path accordingly
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
-import LargeCard from "../components/LargeCard.jsx";
+
 import SmallCard from "../components/SmallCard.jsx";
 import ScheduleRounds from "../components/ScheduleRounds.jsx";
 import "../styles/Home.css";
@@ -111,9 +111,7 @@ const Home = () => {
         <div className="smallCard">
           <SmallCard />
         </div>
-        <div className="largeCard">
-          <LargeCard />
-        </div>
+       
         <ScheduleRounds />
         <Footer />
       </div>
